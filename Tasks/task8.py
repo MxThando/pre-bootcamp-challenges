9cff7ec7c47d853bd92ffe6e52012c27ed6d469b
MxThando <MxThando@users.noreply.github.com>
2020-11-14 14:56:57 +0200

    Update task8.py
    added more changes to the print function if the answer has no hours

--- Tasks/task8.py
+++ Tasks/task8.py
@@ -4,7 +4,9 @@ hour = number // 60
 
 minute = number % 60
 
-if hour <= 1:
+if hour < 1:
+    print(f"{minute} minutes")
+elif hour == 1:
     print(hour , f"hour and {minute} minutes")
 else:
     print(hour , f"hours and {minute} minutes")
