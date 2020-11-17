2636ad505deab8eeb65ea9889d7edabfb93a5124
MxThando <MxThando@users.noreply.github.com>
2020-11-17 14:42:47 +0200

    changes to code as reviewed by Siphiwe
    fixed print statement in task4
    
    fixed code naming in task7

--- Tasks/task7.py
+++ Tasks/task7.py
@@ -1,12 +1,12 @@
 #conversion from celcius to farenheit
-def celcius(conversion1):
-    conversion1 = int((conversion1*1.8) + 32)
-    return(conversion1)
+def celcius_to_farenheit(conversion):
+    conversion = int((conversion*1.8) + 32)
+    return(conversion)
         
-celcius(32)
+celcius_to_farenheit(32)
 
 #conversion from farenhight to degrees
-def farenheit(conversion2):
-    conversion2 = int(((conversion2-32)*5)/9)
-    return(conversion2,)
-farenheit(100)
+def farenheit_to_calcius(conversion):
+    conversion = int(((conversion-32)*5)/9)
+    return(conversion,)
+farenheit_to_calcius(100)
