93393673240d436b5eb26b83b3d627ddeb378863
mxthando <tmsutwana@gmail.com>
2020-12-15 21:33:24 +0200

    fixed tasks 11 and 7

--- Tasks/task11.py
+++ Tasks/task11.py
@@ -1,10 +1,12 @@
 def task11(word1,word2):
-    
+
     word1 = [item.lower() for item in word1]
     word2 = [item.lower() for item in word2]
-    
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
     for i in word1:
         if i in word2:
-            print(i)
+            if i in vowels:
+                print(i)
 
 task11('ThandOu','MapHeUo')
--- Tasks/task7.py
+++ Tasks/task7.py
@@ -1,9 +1,10 @@
-def celcius_to_farenheit(conversion):
+def conversion_from_celcius_to_farenheit(conversion):
     conversion = int((conversion*1.8) + 32)
     return conversion
-celcius_to_farenheit(32)
 
-def farenheit_to_calcius(conversion):
+def conversion_from_farenheit_to_calcius(conversion):
     conversion = int(((conversion-32)*5)/9)
     return conversion
-farenheit_to_calcius(100)
+if __name__ == '__main__':
+    print(conversion_from_celcius_to_farenheit(32))
+    print(conversion_from_farenheit_to_calcius(100))
