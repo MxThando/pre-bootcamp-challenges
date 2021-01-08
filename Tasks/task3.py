096268177ccc897fccf3019326b479d2c6d53da9
mxthando <tmsutwana@gmail.com>
2021-01-08 09:10:30 +0200

    Changes to task3.py using 4spaces instead of tab spaces

--- Tasks/task3.py
+++ Tasks/task3.py
@@ -1,12 +1,9 @@
 def addition(x,y):
-	
-	sum = (x + y)
-	
-	if x == 65 or y == 65:
-		return(True)
-		
-	elif sum == 65:
-		return True
-	else:
-		return False
+    sum = (x + y)
+    if x == 65 or y == 65:
+        return(True)
+    elif sum == 65:
+        return True
+    else:
+        return False
 addition(65,5)
