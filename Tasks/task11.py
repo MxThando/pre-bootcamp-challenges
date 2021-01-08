177ae307b9198caee6512cd5decce47373b9c48e
mxthando <tmsutwana@gmail.com>
2021-01-08 11:00:05 +0200

    made changes to task11

--- Tasks/task11.py
+++ Tasks/task11.py
@@ -2,12 +2,15 @@ def task11(word1,word2):
 
     word1 = [item.lower() for item in word1]
     word2 = [item.lower() for item in word2]
-    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    store = []
 
     for i in word1:
-        if i in word2:
-            if i in vowels:
-                print(i)
+        for j in word2:
+            if i not in store and i == j:
+                store.append(i)
+    store = ",".join(store)
+    print(f"Common letters {store}")
 
 if __name__ == '__main__':
     task11('ThandOu','MapHeUo')
