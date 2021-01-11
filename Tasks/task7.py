e7ddfb8e105532c429f823ae30aa72a42d1c37ba
mxthando <tmsutwana@gmail.com>
2021-01-11 08:43:01 +0200

    made changes to variable names

--- Tasks/task7.py
+++ Tasks/task7.py
@@ -1,9 +1,9 @@
-def conversion_from_celcius_to_farenheit(conversion):
-    conversion = int((conversion*1.8) + 32)
+def conversion_from_celcius_to_farenheit(temperature):
+    conversion = int((temperature*1.8) + 32)
     return conversion
 
-def conversion_from_farenheit_to_calcius(conversion):
-    conversion = int(((conversion-32)*5)/9)
+def conversion_from_farenheit_to_calcius(temperature):
+    conversion = int(((temperature-32)*5)/9)
     return conversion
 if __name__ == '__main__':
     print(conversion_from_celcius_to_farenheit(32))
